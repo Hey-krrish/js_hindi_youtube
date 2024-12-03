7e4de555b280cb5f8f70a4e8836f70ddbb7a11c8
Hey-krrish <krrishranjan01@gmail.com>
2024-12-03 14:29:46 +0000

    convertion_complete

---
+++ 01_basic/convertion.js
@@ -0,0 +1,17 @@
+let a=23;
+let b;
+let c="kirti";
+let x="34"
+let y =23;
+let z=x+y;
+let p=true
+
+
+
+console.log(typeof(a))
+console.log(typeof(""))
+console.log(typeof(b))
+console.log(typeof(a+c))
+console.log(typeof(z))
+console.log(typeof(p))
+
